6a56524e83f104f223ed39df54b66080efece92d
mirzaatalhaa <144937151+mirzaatalhaa@users.noreply.github.com>
2025-01-25 12:36:47 +0530

    Update gradient_container.dart

--- gradient_container.dart
+++ gradient_container.dart
@@ -28,21 +28,3 @@ class GradientContainer extends StatelessWidget {
   }
 }
 
-/* class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.colors});
-
-  final List<Color> colors;
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: colors,
-          begin: startAlignment,
-          end: endAlignment,
-        ),
-      ),
-      child: const Center(child: StyledText('Hello World!')),
-    );
-  }
-} */
